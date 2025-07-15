504d764c54f2270be7b2432203dd498170e3fd27
TheBugMaker <thebugmaker.com>
2025-07-15 08:22:55 +0800

    i put the a color rect on the main menu and added button sinals to the script

---
+++ scripts/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
