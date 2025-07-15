da2ed772c852dbe2c3fab0d0cc037756a18fe764
TheBugMaker <thebugmaker.com>
2025-07-15 11:21:51 +0800

    added some nodes and put all my theme to the other scenes

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+func _ready() -> void:
+	pass
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
@@ -14,3 +16,6 @@ func _on_credits_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+	
+	
+	
