992b13b31ab42234d415a510458d911e62088e11
TheBugMaker <thebugmaker.com>
2025-07-15 08:51:14 +0800

    i update the theme for panel contaner and i did the credits

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -10,7 +10,7 @@ func _on_options_pressed() -> void:
 
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
 
 func _on_quit_pressed() -> void:
