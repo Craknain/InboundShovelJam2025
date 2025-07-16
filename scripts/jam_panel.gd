6ba5b5ed4ed146e110ce4423bbac18459f012050
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-07-17 01:34:07 +0200

    Added type check on area entered

--- scripts/jam_panel.gd
+++ scripts/jam_panel.gd
@@ -1,16 +1,18 @@
 extends PanelContainer
 
-var dragged_object = null
+var dragged_object : Export = null
 
 var game_name : String
 
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_released("click") and dragged_object:
+		# submit_project()
 		dragged_object.queue_free()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	dragged_object = area.get_parent()
+	if area.get_parent() is Export:
+		dragged_object = area.get_parent()
 	
 	#area.get_parent().queue_free()
 	
@@ -20,5 +22,6 @@ func _on_area_2d_area_exited(area: Area2D) -> void:
 	dragged_object = null
 	
 func submit_project():
+	# Need to test if dragged_object is the one needed, and grant or lose points according to result
 	if dragged_object.game_theme == "test":
 		pass
